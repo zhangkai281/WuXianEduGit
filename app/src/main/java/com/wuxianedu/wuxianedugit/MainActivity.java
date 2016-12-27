2dc1dc28e406a72edb0294b73f22d9f9feb98211
lkylky123 <lkylky123>
2016-12-27 16:39:07 +0800

    萨顶顶撒

--- app/src/main/java/com/wuxianedu/wuxianedugit/MainActivity.java
+++ app/src/main/java/com/wuxianedu/wuxianedugit/MainActivity.java
@@ -20,6 +20,9 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
 //            @Override

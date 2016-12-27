a960b598db39439bcfba241186a6b7d435944916
zhangkai281 <zhangkai281@sina.com>
2016-12-27 16:15:25 +0800

    增加 测试类

--- app/src/androidTest/java/com/wuxianedu/wuxianedugit/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/wuxianedu/wuxianedugit/ExampleInstrumentedTest.java
@@ -22,5 +22,8 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.wuxianedu.wuxianedugit", appContext.getPackageName());
+        assertEquals("com.wuxianedu.wuxianedugit", appContext.getPackageName());
+        assertEquals("com.wuxianedu.wuxianedugit", appContext.getPackageName());
+        assertEquals("com.wuxianedu.wuxianedugit", appContext.getPackageName());
     }
 }
